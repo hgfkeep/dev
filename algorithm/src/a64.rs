08f19e07d9c1a6699d7d25abed8ca7f37e0cba08
GF He <hgfkeep@outlook.com>
2020-06-03 03:15:45 +0000

    完成64题

--- algorithm/src/a64.rs
+++ algorithm/src/a64.rs
@@ -1,1 +1,29 @@
 use crate::Solution;
+
+/// 无法使用判断语句，循环语句等
+impl Solution {
+    ///正常解法
+    pub fn sum_nums_nomal(n: i32) -> i32 {
+        // rust immutable value n
+        let mut x = n;
+        if n > 0 {
+            x += Solution::sum_nums(n - 1);
+        }
+        x
+    }
+
+    /// rust unstalble 解法
+    pub fn sum_nums_unstable(n: i32) -> i32 {
+        let mut x = n;
+        let _ = (n > 0).then(|| x += Solution::sum_nums(n - 1));
+        x
+    }
+
+    /// 合理的解法
+    pub fn sum_nums(n: i32) -> i32 {
+        // rust immutable value n
+        let mut x = n;
+        let _ = n > 0 && (x += Solution::sum_nums(n - 1)) == ();
+        x
+    }
+}
--- algorithm/src/main.rs
+++ algorithm/src/main.rs
@@ -1,7 +1,8 @@
+#![feature(bool_to_option)]
 mod a64;
 
 struct Solution;
 
 fn main() {
-    println!("Hello, world!");
+    println!("res: {:?}", Solution::sum_nums(3));
 }
